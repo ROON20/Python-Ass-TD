79b5a5f11e3b72bcaae17f626702d88f56b29579
Swarnava Pal <116735961+ROON20@users.noreply.github.com>
2025-07-16 00:41:27 +0530

    Math module.py

---
+++ Ass-3/Math module.py
@@ -0,0 +1,14 @@
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+
+log_val = math.log(num)
+
+sine_val = math.sin(num)
+
+print("\nCalculated Results:")
+print("Square root of", num, "is:", sqrt_val)
+print("Natural logarithm (ln) of", num, "is:", log_val)
+print("Sine of", num, "(radians) is:", sine_val)
