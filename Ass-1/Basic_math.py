4aaa53057f6af64a16417349c7ea2a8395db6c34
Swarnava Pal <116735961+ROON20@users.noreply.github.com>
2025-07-15 23:40:52 +0530

    Basic_math.py

---
+++ Ass-1/Basic_math.py
@@ -0,0 +1,12 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {div}")
