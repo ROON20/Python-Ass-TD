65fd83be3d396b6439f8253c38da55505137bfdf
Swarnava Pal <116735961+ROON20@users.noreply.github.com>
2025-07-17 21:27:01 +0530

    Demonstrate List Slicing.py

---
+++ Ass-5/Demonstrate List Slicing.py
@@ -0,0 +1,8 @@
+numbers = list(range(1,11))
+print("original list: ",numbers)
+
+f_five = numbers[:5]
+print("Extracted first five element: ",f_five)
+
+r_five = numbers[:4:-1]
+print("Reversed extracted element: ",r_five)
